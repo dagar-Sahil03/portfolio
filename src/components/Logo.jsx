5ba2f0f93ad8e71a9b778314c077a34120785afe
Sahil dagar <sahil.dagar@mindrops.com>
2025-04-16 16:34:33 +0530

    Image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export const Logo = () => {
   return (
     <LOGO>
-        <img src="/Logo.png" alt="Sahil Logo" />
+        <img src="%PUBLIC_URL%/Logo.png" alt="Sahil's Logo" />
     </LOGO>
   )
 }
